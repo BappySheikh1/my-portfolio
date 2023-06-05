5aa715958b1d99c993cdeb1ff690fcaedf506e55
Your Name Main <you@example.com>
2023-06-05 01:24:20 -0700

    modify added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,25 +3,31 @@ import './header.css'
 import Me from '../../assets/bappy.png'
 import HeaderSocials from './HeaderSocials';
 
+
 const Header = () => {
+
+
     return (
-        <>
-            <header>
-                <div className="container header_container">
-                    <h4>Hello I`m</h4>
-                    <h1>Bappy Sheikh</h1>
-                    <h5 className='text-light'>MERN STACK DEVELOPER</h5>
-                    <CTA />
-                    <HeaderSocials />
+      <>
+        <header>
+          <div className="container header_container">
+            <h4>Hello I`m</h4>
+            <h1>Bappy Sheikh</h1>
+            <h5 className="text-light">MERN STACK DEVELOPER</h5>
+            
+            <CTA />
+            <HeaderSocials />
 
-                    <div className="me">
-                        <img src={Me} alt="" />
-                    </div>
+            <div className="me">
+              <img src={Me} alt="" />
+            </div>
 
-                    <a href="#contact" className='scroll_down'>Scroll Down</a>
-                </div>
-            </header>
-        </>
+            <a href="#contact" className="scroll_down">
+              Scroll Down
+            </a>
+          </div>
+        </header>
+      </>
     );
 };
 
